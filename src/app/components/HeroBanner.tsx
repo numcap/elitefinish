366869e3ab742bd06d62c24caf0602b54e33c4cd
Jonathan <juno9593@gmail.com>
2025-07-14 16:37:01 -0400

    feat: Update HeroBanner component to use new image

--- src/app/components/HeroBanner.tsx
+++ src/app/components/HeroBanner.tsx
@@ -6,7 +6,7 @@ const HeroBanner = () => {
 		<div className='relative w-full h-[40rem]'>
 			<Image
 				className='absolute top-0 left-0 object-cover h-full w-full opacity-60'
-				src='/IMG_7763.jpg'
+				src='/IMG_7631 copy.jpg'
 				alt='Photo'
 				width={1656}
 				height={813}
@@ -19,7 +19,12 @@ const HeroBanner = () => {
 				<h1 className='text-white md:px-36 px-5 mb-24 text-4xl md:text-6xl text-center 2xl:w-[60%]'>
 					Local Interior & Exterior Car Detailing in London, Ontario
 				</h1>
-                <Link href="/contact" className="text-3xl py-4 px-5 bg-[#1589CF] hover: rounded-3xl cursor-pointer">Get a Quote</Link>
+				<Link
+					href='/contact'
+					className='text-3xl py-4 px-5 bg-[#1589CF] hover: rounded-3xl cursor-pointer'
+				>
+					Get a Quote
+				</Link>
 			</div>
 		</div>
 	);
